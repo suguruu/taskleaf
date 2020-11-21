2fba79c838617c9d262ed33e47512f3f99526647
Suguru <sugar961216@gmail.com>
2020-11-21 13:23:31 +0900

    ch

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30  }
+  validate :validate_name_not_including_comma
+
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
-  post '/login' to: 'session#create'
+  post '/login', to: 'session#create'
   delete '/logout', to: 'session#destroy'
 
   namespace :admin do
